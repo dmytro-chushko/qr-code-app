79da0baa55841ad340d13258e62a4e8665c156ca
dmytro-chushko <dmytro.chushko@gmail.com>
2024-06-27 23:58:28 +0300

    Return a JSON Response

--- app/routes/app.qrcodes.$id.js
+++ app/routes/app.qrcodes.$id.js
@@ -29,3 +29,16 @@ import { ImageIcon } from "@shopify/polaris-icons";
 
 import db from "../db.server";
 import { getQRCode, validateQRCode } from "../models/QRCode.server";
+
+export async function loader({ request, params }) {
+  const { admin } = await authenticate.admin(request);
+
+  if (params.id === "new") {
+    return json({
+      destination: "product",
+      title: "",
+    });
+  }
+
+  return json(await getQRCode(Number(params.id), admin.graphql));
+}
