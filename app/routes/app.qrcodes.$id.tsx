cd6d6aa27d3e30d02edb539c559ab9695b955ddd
dmytro-chushko <dmytro.chushko@gmail.com>
2024-07-20 19:52:13 +0300

    Add Form

--- app/routes/app.qrcodes.$id.tsx
+++ app/routes/app.qrcodes.$id.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { ActionFunctionArgs, LoaderFunctionArgs, json, redirect } from "@remix-run/node";
+import type { ActionFunctionArgs, LoaderFunctionArgs} from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import {
   useActionData,
   useLoaderData,
@@ -73,7 +74,7 @@ export async function action({ request, params }: ActionFunctionArgs) {
 }
 
 export default function QRCodeForm() {
-  const errors = useActionData<typeof action>()?.errors || {};
+  const errors = useActionData<typeof action>()?.errors;
 
   const qrCode = useLoaderData<typeof loader>();
   const [formState, setFormState] = useState(qrCode);
@@ -122,3 +123,158 @@ export default function QRCodeForm() {
     setCleanFormState({ ...formState });
     submit(data, { method: "post" });
   }
+
+  return (
+    <Page>
+      <ui-title-bar title={qrCode.id ? "Edit QR code" : "Create new QR code"}>
+        <button variant="breadcrumb" onClick={() => navigate("/app")}>
+          QR codes
+        </button>
+      </ui-title-bar>
+      <Layout>
+        <Layout.Section>
+          <BlockStack gap="500">
+            <Card>
+              <BlockStack gap="500">
+                <Text as={"h2"} variant="headingLg">
+                  Title
+                </Text>
+                <TextField
+                  id="title"
+                  helpText="Only store staff can see this title"
+                  label="title"
+                  labelHidden
+                  autoComplete="off"
+                  value={formState.title}
+                  onChange={(title) => setFormState({ ...formState, title })}
+                  error={errors?.title}
+                />
+              </BlockStack>
+            </Card>
+            <Card>
+              <BlockStack gap="500">
+                <InlineStack align="space-between">
+                  <Text as={"h2"} variant="headingLg">
+                    Product
+                  </Text>
+                  {formState.productId ? (
+                    <Button variant="plain" onClick={selectProduct}>
+                      Change product
+                    </Button>
+                  ) : null}
+                </InlineStack>
+                {formState.productId ? (
+                  <InlineStack blockAlign="center" gap="500">
+                    <Thumbnail
+                      source={formState.productImage || ImageIcon}
+                      alt={formState.productAlt}
+                    />
+                    <Text as="span" variant="headingMd" fontWeight="semibold">
+                      {formState.productTitle}
+                    </Text>
+                  </InlineStack>
+                ) : (
+                  <BlockStack gap="200">
+                    <Button onClick={selectProduct} id="select-product">
+                      Select product
+                    </Button>
+                    {errors.productId ? (
+                      <InlineError
+                        message={errors.productId}
+                        fieldID="myFieldID"
+                      />
+                    ) : null}
+                  </BlockStack>
+                )}
+                <Bleed marginInlineStart="200" marginInlineEnd="200">
+                  <Divider />
+                </Bleed>
+                <InlineStack gap="500" align="space-between" blockAlign="start">
+                  <ChoiceList
+                    title="Scan destination"
+                    choices={[
+                      { label: "Link to product page", value: "product" },
+                      {
+                        label: "Link to checkout page with product in the cart",
+                        value: "cart",
+                      },
+                    ]}
+                    selected={[formState.destination]}
+                    onChange={(destination) =>
+                      setFormState({
+                        ...formState,
+                        destination: destination[0],
+                      })
+                    }
+                    error={errors.destination}
+                  />
+                  {qrCode.destinationUrl ? (
+                    <Button
+                      variant="plain"
+                      url={qrCode.destinationUrl}
+                      target="_blank"
+                    >
+                      Go to destination URL
+                    </Button>
+                  ) : null}
+                </InlineStack>
+              </BlockStack>
+              </Card>
+          </BlockStack>
+        </Layout.Section>
+        <Layout.Section variant="oneThird">
+        <Card>
+            <Text as={"h2"} variant="headingLg">
+              QR code
+            </Text>
+            {qrCode ? (
+              <EmptyState image={qrCode.image} imageContained={true} />
+            ) : (
+              <EmptyState image="">
+                Your QR code will appear here after you save
+              </EmptyState>
+            )}
+            <BlockStack gap="300">
+              <Button
+                disabled={!qrCode?.image}
+                url={qrCode?.image}
+                download
+                variant="primary"
+              >
+                Download
+              </Button>
+              <Button
+                disabled={!qrCode.id}
+                url={`/qrcodes/${qrCode.id}`}
+                target="_blank"
+              >
+                Go to public URL
+              </Button>
+            </BlockStack>
+          </Card>
+        </Layout.Section>
+        <Layout.Section>
+          <PageActions
+            secondaryActions={[
+              {
+                content: "Delete",
+                loading: isDeleting,
+                disabled: !qrCode.id || !qrCode || isSaving || isDeleting,
+                destructive: true,
+                outline: true,
+                onAction: () =>
+                  submit({ action: "delete" }, { method: "post" }),
+              },
+            ]}
+            primaryAction={{
+              content: "Save",
+              loading: isSaving,
+              disabled: !isDirty || isSaving || isDeleting,
+              onAction: handleSave,
+            }}
+          />
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+}
